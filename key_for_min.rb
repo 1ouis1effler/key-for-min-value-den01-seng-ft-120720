6b1581f8cf65bc9a20099c52f343e01f68bf73e2
Louis Leffler <louisleffler@gmail.com>
2020-11-29 21:24:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,14 @@ def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil
   name_hash.each do |key, value|
-    if min_value == nil || < min_value
+    if min_value == nil
       min_key = key
       min_value = value
+    elsif_value < min_value
     min_key = key
     min_value = value 
     end 
   end 
   
-  min_key 
+  min_key
 end
